c0cf424b0b293dfdc37df30479a3f2ea88df94a2
Yi <yi@ohiofilmgroup.com>
2018-01-16 16:12:53 -0500

    Remove some test scripts and folders in repo

--- python/tk_desktop_timecard/testScraper.py
+++
@@ -1,8 +0,0 @@
-import requests
-import os
-from selenium import webdriver
-
-browser = webdriver.Chrome("webdriver\chromedriver.exe")
-hostname = os.environ.get("COMPUTERNAME", "unknown")
-url = "http://localhost:5600/#/activity/%s" % hostname
-browser.get(url)
